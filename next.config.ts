63fcbf2eac31c91b135ecd4ff9880621536fdb81
David Xiong <dzyxiong12@gmail.com>
2025-06-08 19:08:39 -0700

    config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
@@ -20,6 +20,6 @@ const nextConfig: NextConfig = {
     ],
     unoptimized: true,
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
